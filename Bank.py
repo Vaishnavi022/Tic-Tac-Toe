b7a1cb1d50ddfeb29a0f9211d935acffa268afcd
Chaudhari Vaishnavi Narendra <138233760+Vaishnavi022@users.noreply.github.com>
2026-04-10 14:58:39 +0530

    Update Bank.py

--- Bank.py
+++ Bank.py
@@ -1,15 +1,1 @@
-class BankAccount:
-     def __init__(self,name,pin):
-             self.name = name
-             self.pin = pin
-     def changePin(self):
-             old_pin = int(input("Enter current Pin: "))
-             if old_pin == self.pin:
-                 new_pin = int(input("Enter new Pin: "))
-                 self.pin = new_pin
-                 print("PIN chnaged Successfully")
-             else:
-                 print("Incorrect PIN")
-
-a = BankAccount("Disha",93)
-a.changePin()
+print("Hello, World!")
